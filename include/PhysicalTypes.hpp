b18d94700d76a3db726f71adb0895535e50ac86c
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2023-06-09 14:10:58 +0100

    Allow implicit construction of SimpleFrac
    For easier definition of common units. There is no context in which
    a SimpleFrac is not the same as the integer its initialised with.

--- include/PhysicalTypes.hpp
+++ include/PhysicalTypes.hpp
@@ -9,28 +9,28 @@ using dbl3vec = STVector<double, 3>;
 using dbl3tens = STTensor<double, 3>;
 
 // No physical units
-using UCScalar = UnitCheckedType<SF{0}, SF{0}, SF{0}, dblscalar>;
-using UCVector = UnitCheckedType<SF{0}, SF{0}, SF{0}, dbl3vec>;
+using UCScalar = UnitCheckedType<0, 0, 0, dblscalar>;
+using UCVector = UnitCheckedType<0, 0, 0, dbl3vec>;
 
 // Single units, scalar and (where sensible), 3-vector
 
-using Length = UnitCheckedType<SF{1}, SF{0}, SF{0}, dblscalar>;
-using Mass = UnitCheckedType<SF{0}, SF{1}, SF{0}, dblscalar>;
-using Time = UnitCheckedType<SF{0}, SF{0}, SF{1}, dblscalar>;
-using Position = UnitCheckedType<SF{1}, SF{0}, SF{0}, dbl3vec>;
+using Length = UnitCheckedType<1, 0, 0, dblscalar>;
+using Mass = UnitCheckedType<0, 1, 0, dblscalar>;
+using Time = UnitCheckedType<0, 0, 1, dblscalar>;
+using Position = UnitCheckedType<1, 0, 0, dbl3vec>;
 
 // Common physical quantities for dynamics
-using Speed = UnitCheckedType<SF{1}, SF{0}, SF{-1}, dblscalar>;
-using Velocity = UnitCheckedType<SF{1}, SF{0}, SF{-1}, dbl3vec>;
-using Momentum = UnitCheckedType<SF{1}, SF{1}, SF{-1}, dbl3vec>;
+using Speed = UnitCheckedType<1, 0, -1, dblscalar>;
+using Velocity = UnitCheckedType<1, 0, -1, dbl3vec>;
+using Momentum = UnitCheckedType<1, 1, -1, dbl3vec>;
 
-using Acceleration = UnitCheckedType<SF{1}, SF{0}, SF{-2}, dbl3vec>;
-using Force = UnitCheckedType<SF{1}, SF{1}, SF{-2}, dbl3vec>;
+using Acceleration = UnitCheckedType<1, 0, -2, dbl3vec>;
+using Force = UnitCheckedType<1, 1, -2, dbl3vec>;
 
-using Energy = UnitCheckedType<SF{2}, SF{1}, SF{-2}, dblscalar>;
+using Energy = UnitCheckedType<2, 1, -2, dblscalar>;
 
 // Common Tensor types
-using StressTensor = UnitCheckedType<SF{-1}, SF{1}, SF{-2}, dbl3tens>;
+using StressTensor = UnitCheckedType<-1, 1, -2, dbl3tens>;
 
 
 #endif
