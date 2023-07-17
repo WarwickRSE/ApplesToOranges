1d7b6e2dafd04f2c7ff43f0f84d893816234d900
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2023-07-17 11:16:17 +0100

    Add vector test/example

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 #include "PhysicalTypes.hpp"
 
 int main(){
@@ -23,7 +24,12 @@ int main(){
   // A time
   Time t{0.1};
   std::cout<<"Defined time t= "<<t<<t.units()<<std::endl;
-  
+
+  std::vector<Time> t_steps;
+  t_steps.push_back(t);
+
+  Time t2(t);
+
   // A position
   Position x{1.0, 2.0, 3.0};  
   std::cout<<"Defined position x= "<<x<<x.units()<<std::endl;
