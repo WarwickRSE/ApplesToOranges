36f3d177f4c004b26dc488778011e94e6cb27225
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-06-28 16:37:49 +0100

    Fix naming
    Misnamed, which adds another useless error message

--- include/UnitCheckedType.hpp
+++ include/UnitCheckedType.hpp
@@ -125,7 +125,7 @@ class UnitCheckedTypeFull{
     ///Copy constructor fake to make a nicer error message if we try and remove the const-ness from a ref
     //NOTE: actually setting val like this is _WRONG_ but with the assert this code will never compile, so we do it to squelch a confusing warning
     template <typename STm, typename=std::enable_if_t<!std::is_same_v<ST, STm> > >
-    constexpr UnitCheckedTypeFull(const UnitCheckedTypeFull<L,M,T,K,A,MO,CD, STm> & in):val(src.val){
+    constexpr UnitCheckedTypeFull(const UnitCheckedTypeFull<L,M,T,K,A,MO,CD, STm> & src):val(src.val){
       if constexpr(STm::is_const_v && !ST::is_const_v){
         static_assert(!STm::is_const_v, "Error: Trying to remove const from a reference!");
       }
